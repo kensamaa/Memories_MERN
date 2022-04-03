a01abfc835fea86bfa3d679f8466afda7ae4eda7
kensamaa <amineab203@gmail.com>
2022-04-03 04:00:34 +0000

    setup db

--- server/db/index.js
+++ server/db/index.js
@@ -1,24 +1,15 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
 import mongoose from "mongoose";
-
-const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-
-//setup db
 const connectionString = "mongodb://localhost:27017/Memories";
 const config = { useunifiedtopology: true, useNewUrlParser: true };
 const DB = mongoose.connection;
 mongoose.connect(connectionString, config).catch((e) => {
   console.error("Connection error", e.message);
 });
-
+mongoose.set("useFindAndModify", false);
 DB.on("open", () => console.log("you are connected to mongo"))
   .on("close", () => console.log("you are disconnected to mongo"))
   .on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const db = mongoose.connection;
+
+module.exports = db;
