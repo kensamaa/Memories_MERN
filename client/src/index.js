99022b1898c1738b2d0630b58317edf8c967bfdd
kensamaa <amineab203@gmail.com>
2022-04-04 02:33:09 +0000

    background for client

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const container = document.getElementById("root");
