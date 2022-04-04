f78299be279268551d984dd66cb32bf258eff8e7
kensamaa <amineab203@gmail.com>
2022-04-04 02:29:34 +0000

    add post works

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./post/Post.js";
 import useStyles from "./styles";
 import { useSelector } from "react-redux";
@@ -6,13 +7,21 @@ const Posts = () => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
   console.log(posts);
-  return (
-    <>
-      {" "}
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 export default Posts;
