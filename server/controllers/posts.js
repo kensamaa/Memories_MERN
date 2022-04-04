c09521829bf84064c50f4e98613db1a27ea71465
kensamaa <amineab203@gmail.com>
2022-04-04 02:38:56 +0000

    add update post api

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -16,6 +17,20 @@ export const createPost = async (req, res) => {
     await newPostMessage.save();
 
     res.status(201).json(newPostMessage);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.bodyl;
+  if (mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("no post with id");
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true,
+    });
+    res.status(201).json(updatedPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
