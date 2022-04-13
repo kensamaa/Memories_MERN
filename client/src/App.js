bd2f5a44ae737b3daf32a70293a4a16168458806
kensamaa <amineab203@gmail.com>
2022-04-13 23:25:12 +0000

    add  react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import memories from "./imgs/memories.png";
-import Posts from "./components/posts/Posts.js";
-import Form from "./components/form/Form.js";
-import useStyles from "./styles";
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
+import React from "react";
+import { Container } from "@material-ui/core";
 
+import Navbar from "./components/navbar/Navbar.js";
+import Home from "./components/Home/Home.js";
+import Auth from "./components/Auth/Auth";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 const App = () => {
-  const classes = useStyles();
-  const dispach = useDispatch();
-  const [currentId, setCurrentId] = useState(null);
-  //dispach actions
-  useEffect(() => {
-    dispach(getPosts());
-  }, [currentId, dispach]);
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            className={classes.mainContainer}
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 export default App;
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Container, Grow, Grid } from "@material-ui/core";
+import Posts from "../posts/Posts";
+import Form from "../form/Form";
+import useStyles from "../../styles";
+import { useDispatch } from "react-redux";
+import { getPosts } from "../../actions/posts";
+
+const Home = () => {
+  const classes = useStyles();
+
+  const dispach = useDispatch();
+  const [currentId, setCurrentId] = useState(null);
+  //dispach actions
+  useEffect(() => {
+    dispach(getPosts());
+  }, [currentId, dispach]);
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          container
+          className={classes.mainContainer}
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
